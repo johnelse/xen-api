b4f3ecb71274ed483d2eb1b243bf93c0a3e16860
John Else <john.else@citrix.com>
2013-07-11 11:13:02 +0100

    Stop hiding GPU_group.create and GPU_group.destroy from the docs
    This also means these methods get added to the bindings.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -7708,7 +7708,6 @@ let gpu_group =
 		]
 		~result:(Ref _gpu_group, "")
 		~allowed_roles:_R_POOL_OP
-		~hide_from_docs:true
 		()
 	in
 	let destroy = call
@@ -7718,7 +7717,6 @@ let gpu_group =
 			Ref _gpu_group, "self", "The vGPU to destroy"
 		]
 		~allowed_roles:_R_POOL_OP
-		~hide_from_docs:true
 		()
 	in
 	let get_allowed_VGPU_types = call
