fb262323fd4d1d1b4b75fca013562a1d07bd4cfc
Si Beaumont <simon.beaumont@citrix.com>
2015-06-27 17:07:32 +0100

    Fix laundering of Bad_power_state exn from Xenopsd
    This exception, defined in Xenops_interface, only has two arguments: the
    expected and actual power states. The code that transformed this into a XenAPI
    error didn't have the VM's identifier to hand and therefore created an
    Api_error with too few arguments. This led to errors like the following being
    returned from the API:
    
      You attempted an operation on a VM that was not in an appropriate power state
      at the time; for example, you attempted to start a VM that was already
      running.  The parameters returned are the VM's handle, and the expected and
      actual VM state at the time of the call.
      vm: Halted
      expected: Running
      actual: <unknown>
    
    This patch fixes this so that Xapi_xenops.transform_xenops_exn always knows the
    ref of the VM related to the operation.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>
    (cherry picked from commit e6e8a638bb588d212c19729b778124cc805187ef)
    
    Conflicts:
    
    	ocaml/xapi/xapi_vm_migrate.ml
    	ocaml/xapi/xapi_xenops.ml
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>
    (cherry picked from commit 7ff6fcc4ccd3f576012a42d162a2ffbd19eb1b77)

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -1725,7 +1725,7 @@ let success_task f dbg id =
 
 (* Catch any uncaught xenops exceptions and transform into the most relevant XenAPI error.
    We do not want a XenAPI client to see a raw xenopsd error. *)
-let transform_xenops_exn ~__context f =
+let transform_xenops_exn ~__context ~vm f =
 	let reraise code params =
 		error "Re-raising as %s [ %s ]" code (String.concat "; " params);
 		raise (Api_errors.Server_error(code, params)) in
@@ -1743,9 +1743,9 @@ let transform_xenops_exn ~__context f =
 		| Domain_not_built -> internal "domain has not been built"
 		| Invalid_vcpus n -> internal "the maximum number of vcpus configured for this VM is currently: %d" n
 		| Bad_power_state(found, expected) ->
-			let f x = x |> (fun x -> Some x) |> xenapi_of_xenops_power_state |> Record_util.power_state_to_string in
+			let f x = xenapi_of_xenops_power_state (Some x) |> Record_util.power_state_to_string in
 			let found = f found and expected = f expected in
-			reraise Api_errors.vm_bad_power_state [ expected; found ]
+			reraise Api_errors.vm_bad_power_state [ Ref.string_of vm; expected; found ]
 		| Failed_to_acknowledge_shutdown_request ->
 			reraise Api_errors.vm_failed_shutdown_ack []
 		| Failed_to_shutdown(id, timeout) ->
@@ -1844,7 +1844,7 @@ let sync __context x =
 	x |> wait_for_task dbg |> success_task (update_debug_info __context) dbg
 
 let pause ~__context ~self =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.pause %s" id;
@@ -1855,7 +1855,7 @@ let pause ~__context ~self =
 		)
 
 let unpause ~__context ~self =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.unpause %s" id;
@@ -1866,7 +1866,7 @@ let unpause ~__context ~self =
 		)
 
 let request_rdp ~__context ~self enabled =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.request_rdp %s %b" id enabled;
@@ -1876,7 +1876,7 @@ let request_rdp ~__context ~self enabled =
 		)
 
 let set_xenstore_data ~__context ~self xsdata =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.set_xenstore_data %s" id;
@@ -1886,7 +1886,7 @@ let set_xenstore_data ~__context ~self xsdata =
 		)
 
 let set_vcpus ~__context ~self n =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.set_vcpus %s" id;
@@ -1907,7 +1907,7 @@ let set_vcpus ~__context ~self n =
 		)
 
 let set_shadow_multiplier ~__context ~self target =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.set_shadow_multiplier %s" id;
@@ -1926,7 +1926,7 @@ let set_shadow_multiplier ~__context ~self target =
 		)
 
 let set_memory_dynamic_range ~__context ~self min max =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			debug "xenops: VM.set_memory_dynamic_range %s" id;
@@ -1937,7 +1937,7 @@ let set_memory_dynamic_range ~__context ~self min max =
 
 let start ~__context ~self paused =
 	let dbg = Context.string_of_task __context in
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			(* For all devices which we want xenopsd to manage, set currently_attached = true
 			   so the metadata is pushed. *)
@@ -2002,7 +2002,7 @@ let start ~__context ~self paused =
 	Xapi_vm_lifecycle.assert_power_state_is ~__context ~self ~expected:(if paused then `Paused else `Running)
 
 let start ~__context ~self paused =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			try
 				start ~__context ~self paused
@@ -2016,7 +2016,7 @@ let start ~__context ~self paused =
 		)
 
 let reboot ~__context ~self timeout =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			assert_resident_on ~__context ~self;
 			let id = id_of_vm ~__context ~self in
@@ -2030,7 +2030,7 @@ let reboot ~__context ~self timeout =
 		)
 
 let shutdown ~__context ~self timeout =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			assert_resident_on ~__context ~self;
 			let id = id_of_vm ~__context ~self in
@@ -2049,7 +2049,7 @@ let shutdown ~__context ~self timeout =
 		)
 
 let suspend ~__context ~self =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			assert_resident_on ~__context ~self;
 			let id = id_of_vm ~__context ~self in
@@ -2094,7 +2094,7 @@ let suspend ~__context ~self =
 
 let resume ~__context ~self ~start_paused ~force =
 	let dbg = Context.string_of_task __context in
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let vdi = Db.VM.get_suspend_VDI ~__context ~self in
 			let disk = disk_of_vdi ~__context ~self:vdi |> Opt.unbox in
@@ -2133,7 +2133,7 @@ let resume ~__context ~self ~start_paused ~force =
 		)
 
 let s3suspend ~__context ~self =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			let dbg = Context.string_of_task __context in
@@ -2143,7 +2143,7 @@ let s3suspend ~__context ~self =
 		)
 
 let s3resume ~__context ~self =
-	transform_xenops_exn ~__context
+	transform_xenops_exn ~__context ~vm:self
 		(fun () ->
 			let id = id_of_vm ~__context ~self in
 			let dbg = Context.string_of_task __context in
@@ -2164,9 +2164,9 @@ let md_of_vbd ~__context ~self =
 	MD.of_vbd ~__context ~vm:(Db.VM.get_record ~__context ~self:vm) ~vbd:(Db.VBD.get_record ~__context ~self)
 
 let vbd_plug ~__context ~self =
-	transform_xenops_exn ~__context
+	let vm = Db.VBD.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VBD.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			Events_from_xapi.wait ~__context ~self:vm;
 			let vbd = md_of_vbd ~__context ~self in
@@ -2186,9 +2186,9 @@ let vbd_plug ~__context ~self =
 		)
 
 let vbd_unplug ~__context ~self force =
-	transform_xenops_exn ~__context
+	let vm = Db.VBD.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VBD.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			let vbd = md_of_vbd ~__context ~self in
 			let dbg = Context.string_of_task __context in
@@ -2204,9 +2204,9 @@ let vbd_unplug ~__context ~self force =
 		)
 
 let vbd_eject_hvm ~__context ~self =
-	transform_xenops_exn ~__context
+	let vm = Db.VBD.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VBD.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			let vbd = md_of_vbd ~__context ~self in
 			info "xenops: VBD.eject %s.%s" (fst vbd.Vbd.id) (snd vbd.Vbd.id);
@@ -2219,9 +2219,9 @@ let vbd_eject_hvm ~__context ~self =
 		)
 
 let vbd_insert_hvm ~__context ~self ~vdi =
-	transform_xenops_exn ~__context
+	let vm = Db.VBD.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VBD.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			let vbd = md_of_vbd ~__context ~self in
 			let disk = disk_of_vdi ~__context ~self:vdi |> Opt.unbox in
@@ -2264,9 +2264,9 @@ let md_of_vif ~__context ~self =
 	MD.of_vif ~__context ~vm:(Db.VM.get_record ~__context ~self:vm) ~vif:(Db.VIF.get_record ~__context ~self)
 
 let vif_plug ~__context ~self =
-	transform_xenops_exn ~__context
+	let vm = Db.VIF.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VIF.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			Events_from_xapi.wait ~__context ~self:vm;
 			let vif = md_of_vif ~__context ~self in
@@ -2294,9 +2294,9 @@ let vm_set_vm_data ~__context ~self =
 		)
 
 let vif_set_locking_mode ~__context ~self =
-	transform_xenops_exn ~__context
+	let vm = Db.VIF.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VIF.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			let vif = md_of_vif ~__context ~self in
 			info "xenops: VIF.set_locking_mode %s.%s" (fst vif.Vif.id) (snd vif.Vif.id);
@@ -2306,9 +2306,9 @@ let vif_set_locking_mode ~__context ~self =
 		)
 
 let vif_unplug ~__context ~self force =
-	transform_xenops_exn ~__context
+	let vm = Db.VIF.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
-			let vm = Db.VIF.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
 			let vif = md_of_vif ~__context ~self in
 			info "xenops: VIF.unplug %s.%s" (fst vif.Vif.id) (snd vif.Vif.id);
@@ -2320,7 +2320,8 @@ let vif_unplug ~__context ~self force =
 		)
 
 let vif_move ~__context ~self network =
-	transform_xenops_exn ~__context
+	let vm = Db.VIF.get_VM ~__context ~self in
+	transform_xenops_exn ~__context ~vm
 		(fun () ->
 			let vm = Db.VIF.get_VM ~__context ~self in
 			assert_resident_on ~__context ~self:vm;
