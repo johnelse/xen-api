49dc35497aa969cfed9ef591788865f3346936e4
John Else <john.else@gmail.com>
2019-03-05 23:42:36 +0000

    Use the right fd

--- ocaml/xapi/import_raw_vdi.ml
+++ ocaml/xapi/import_raw_vdi.ml
@@ -108,7 +108,7 @@ let localhost_handler rpc session_id vdi_opt (req: Request.t) (s: Unix.file_desc
                     let consumer pipe_out feeder_thread = finally
                       (fun () ->
                         debug "import_raw_vdi: starting receive";
-                        receive s;
+                        receive pipe_out;
                         debug "import_raw_vdi: finished receive")
                       (fun () ->
                         ignore_exn (fun () -> Unix.close pipe_out);
